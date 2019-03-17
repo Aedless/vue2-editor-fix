072c3cdb8da9a56dc744cf2e983883f78c76f3bd
Aedless <lianyuts@163.com>
2019-03-17 22:16:16 +0800

    fix

--- src/VueEditor.vue
+++ src/VueEditor.vue
@@ -15,6 +15,16 @@ import MarkdownShortcuts from "./helpers/markdown-shortcuts";
 
 const Quill = window.Quill || _Quill;
 
+const generateUUID = () => {
+  let d = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+  });
+  return uuid;
+}
+
 export default {
   name: "VueEditor",
   mixins: [oldApi],
@@ -171,16 +181,6 @@ export default {
       let range = Editor.getSelection();
       let cursorLocation = range.index;
       this.$emit("imageAdded", file, Editor, cursorLocation, resetUploader);
-    },
-
-    generateUUID() {
-     let d = new Date().getTime();
-      const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
-     });
-     return uuid;
     }
   },
 
